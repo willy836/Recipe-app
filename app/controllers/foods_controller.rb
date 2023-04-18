074f45dfa8a714b0459579d2761e95ef77809c7e
willy836 <wilsonkimiri836@gmail.com>
2023-04-18 19:28:40 +0300

    Fix rubocop errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,40 +1,40 @@
 class FoodsController < ApplicationController
-    before_action :set_user, only: [:index, :new]
+  before_action :set_user, only: %i[index new]
 
-    def index
-        @foods = @user.foods
+  def index
+    @foods = @user.foods
+  end
+
+  def new
+    @food = @user.foods.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully added.'
+    else
+      flash.now[:alert] = 'Failed to add food.'
+      render :new
     end
+  end
 
-    def new
-        @food = @user.foods.new
+  def destroy
+    @food = current_user.foods.find(params[:id])
+    if @food.destroy
+      redirect_to foods_path, notice: 'Food deleted successfully.'
+    else
+      redirect_to foods_path, notice: 'An error occurred when deleting the food.'
     end
+  end
 
-    def create
-        @food = current_user.foods.new(food_params)
-        if @food.save
-            redirect_to foods_path, notice: 'Food was successfully added.'
-        else
-            flash.now[:alert] = 'Failed to add food.'
-            render :new
-        end
-    end
+  private
 
-    def destroy
-        @food = current_user.foods.find(params[:id])
-        if @food.destroy
-            redirect_to foods_path, notice: 'Food deleted successfully.'
-        else
-            redirect_to foods_path, notice: 'An error occurred when deleting the food.'
-        end
-    end
+  def set_user
+    @user = User.includes(:foods).find(current_user.id)
+  end
 
-    private
-
-    def set_user
-        @user = User.includes(:foods).find(current_user.id)
-    end
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
