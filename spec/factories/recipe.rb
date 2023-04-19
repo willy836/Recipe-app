a460d1223c43ca33dd2ebbae495a778d22c92d91
willy836 <wilsonkimiri836@gmail.com>
2023-04-19 16:13:40 +0300

    Add unit tests for recipefood model

---
+++ spec/factories/recipe.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+    factory :recipe do
+      name { 'Recipe' }
+      preparation_time { 2 }
+      cooking_time { 4 }
+      description { 'Description' }
+      public { false }
+      
+      trait :user do
+        user
+      end
+    end
+  end
