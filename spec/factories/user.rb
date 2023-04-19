aaf4e0ab34a7b2161eebeb1d0aaa34547aceac3b
willy836 <wilsonkimiri836@gmail.com>
2023-04-19 18:45:23 +0300

    Add user factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "User #{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
+
+    trait :signed_up do
+      password { 'password' }
+      password_confirmation { 'password' }
+    end
+  end
+end
