b95e354129d3c3c615eb308da85d0522d51ff1f1
willy836 <wilsonkimiri836@gmail.com>
2023-04-19 16:20:03 +0300

    Fix rubocop errors

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-    factory :food do
-      name { 'Foodname' }
-      measurement_unit { 'units' }
-      price { '5.99' }
-      
-      trait :user do
-        user
-      end
+  factory :food do
+    name { 'Foodname' }
+    measurement_unit { 'units' }
+    price { '5.99' }
+
+    trait :user do
+      user
     end
   end
+end
