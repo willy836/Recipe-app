00c897e0cb59cee46c15a3c4dc4f2257b4071975
willy836 <wilsonkimiri836@gmail.com>
2023-04-20 08:36:49 +0300

    Fix rubocop errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,13 +62,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 require 'factory_bot_rails'
 ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
