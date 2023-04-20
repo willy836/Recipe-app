0ec89acdeffb689aa8a4688a128357b5ae2b119d
willy836 <wilsonkimiri836@gmail.com>
2023-04-20 13:35:50 +0300

    Add basic styles for login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'recipe_foods/create'
   get 'recipe_foods/destroy'
   devise_for :users
-  root to: "foods#index"
+  root to: "users#index"
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes do 
     resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
